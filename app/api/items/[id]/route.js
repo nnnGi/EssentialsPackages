0eb2d77469e6933c2144495ae29f684c06cc1ab2
Nathanael Chia <natchia.edu@gmail.com>
2025-08-23 21:28:25 +0800

    Modified Path for Imports from lib/db.js

--- app/api/items/[id]/route.js
+++ app/api/items/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { pool } from "../../../../lib/db";
+import { pool } from "@/lib/db";
 
 // GET /api/items/:id
 export async function GET(req, { params }) {
